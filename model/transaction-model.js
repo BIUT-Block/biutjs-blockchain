accc574746e1e24dffd3edbd9df88cc792934da7
theFay <fei.minhao@gmail.com>
2018-05-04 18:46:59 +0200

    create transaction model

---
+++ model/transaction-model.js
@@ -0,0 +1,20 @@
+var transactionModel = {
+    transHash: '',
+    transtimeStamp: '',
+    productName: '',
+    productCategory: '',
+    productDealer: '',
+    shopName: '',
+    productBuyer: '',
+    productCode: '',
+    price: '',
+    currency: '',
+    productDetail: '',
+    sharedTimes: 0,
+    commentar: ''
+}
+
+
+
+
+module.export = transactionModel;
