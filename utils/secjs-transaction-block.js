dc31f27efac3a6aa3256bfaee31162896592a9c3
Zu <zuweihan1992@gmail.com>
2018-05-12 18:44:39 +0200

    Tx block class draft implementation

--- utils/secjs-transaction-block.js
+++ utils/secjs-transaction-block.js
@@ -1,7 +1,48 @@
-class SECTransactionBlock{
-    constructor(){
+const txBlockModel = require('../model/transactionchain-block-model')
+const powCal = require('pow-calculation')
+const hash = require('./hash.js')
 
+class SECTransactionBlock{
+    constructor(config){
+		this.config = config
+		this.transactions = []
+		this.block = new txBlockModel();
     }
+
+	generateBlock(txPool, txBlockChain){
+		collectTxFromPool(txPool)
+		fillInBlockInfo(txBlockChain)
+	}
+
+	collectTxFromPool(txPool){
+		txBuffer = txPool.getAllTxFromPool()
+		txBuffer.foreach((currTx) => {
+			if(verifyTransaction(currTx)) {
+				this.transactions.add(currTx)
+			} else{
+				//report???
+			}
+		});
+	}
+	
+	fillInBlockInfo(txBlockChain){
+		this.block.Height = txBlockChain.currentHeight + 1
+		this.block.TimeStamp = new Date().getTime();
+		this.block.Transactions = this.transactions
+		this.block.Parent_Hash = txBlockChain.lastBlockHash
+		this.block.Mined_By = this.config.userAddr
+		this.block.Block_Reward = 10 		//TBD
+		this.block.Extra_Data = ""			//Empty?
+		
+		this.block.Size = size(this.block) + 2*hash_length
+		this.block.Hash = hash.hash("sha256", this.block)
+		this.block.Nonce = powCal.getNonce(this.block)
+	}
+
+	verifyTransaction(){
+		//do nothing, will be implemented in the future
+		return true
+	}
 }
 
 module.exports = SECTransactionBlock;
