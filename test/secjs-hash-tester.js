7082ada16f6bb8c3e4872230e2d45e9f80ba29dd
theFay <fei.minhao@gmail.com>
2018-05-21 12:07:14 +0200

    test for hash generate finished

--- test/secjs-hash-tester.js
+++ test/secjs-hash-tester.js
@@ -21,8 +21,34 @@ describe('secjs_hash', () => {
     });
 
     describe('get lenght of generated hash', () => {
-        
+        it('return lenght of md5', () => {
+            let secjs_hash_test = new secjs_hash('md5');
+            let hashlength = secjs_hash_test.getHashLength();
+            expect(hashlength).to.equal(16);
+        });
+
+        it('return lenght of sha1', () => {
+            let secjs_hash_test = new secjs_hash('sha1');
+            let hashlength = secjs_hash_test.getHashLength();
+            expect(hashlength).to.equal(20);
+        });
+
+        it('return length of sha256', () => {
+            let secjs_hash_test = new secjs_hash('sha256');
+            let hashlength = secjs_hash_test.getHashLength();
+            expect(hashlength).to.equal(32);
+        });
+
+        it('return length of sha512', () => {
+            let secjs_hash_test = new secjs_hash('sha512');
+            let hashlength = secjs_hash_test.getHashLength();
+            expect(hashlength).to.equal(64);
+        });
+
+        it('return length of ripemd512', () => {
+            let secjs_hash_test = new secjs_hash('ripemd160');
+            let hashlength = secjs_hash_test.getHashLength();
+            expect(hashlength).to.equal(20);
+        });
     });
-
-
 });
