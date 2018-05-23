7b66171804976a113c76e3cf42010c6442c473d2
theFay <fei.minhao@gmail.com>
2018-05-23 22:13:30 +0200

    add new tester

---
+++ test/secjs-transaction-pool-tester.js
@@ -0,0 +1,13 @@
+let secjs_trans_pool = require('../utils/secjs-transaction-pool');
+
+const testconfig = {
+    
+};
+
+describe('transaction pool', () => {
+    describe('add transaction in pool', () => {
+        it('shoud add transaction in pool', () => {
+
+        });
+    });
+});
