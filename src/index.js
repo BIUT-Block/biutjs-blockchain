8dc7bb6bfdef5be58dd6c0cc41cb87da0e614981
Zu <zuweihan1992@gmail.com>
2018-08-05 17:28:20 +0200

    transaction pool related files immigrated to new repository

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const SECTokenBlock = require('./secjs-token-block')
 const SECTokenBlockChain = require('./secjs-token-blockchain')
 const SECTransactionBlock = require('./secjs-transaction-block')
 const SECTransactionBlockChain = require('./secjs-transaction-blockchain')
-const TransactionPool = require('./secjs-transaction-pool')
 
 module.exports = {
   SECHash: SECHash,
@@ -13,5 +12,4 @@ module.exports = {
   SECTokenBlockChain: SECTokenBlockChain,
   SECTransactionBlock: SECTransactionBlock,
   SECTransactionBlockChain: SECTransactionBlockChain,
-  TransactionPool: TransactionPool
 }
