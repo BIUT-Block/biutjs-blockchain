fc19bbf259cad7b05d92b41d4d79c3e811b76540
Zu Weihan <zuweihan1992@gmail.com>
2019-04-10 23:06:16 +0200

    put genesis blocks into one separate file

--- src/secjs-token-blockchain.js
+++ src/secjs-token-blockchain.js
@@ -1,6 +1,7 @@
 const _ = require('lodash')
 const async = require('async')
 const AccTreeDB = require('./secjs-accTree.js')
+const geneData = require('./genesisBlock.js')
 const SECUtils = require('@sec-block/secjs-util')
 const SECTokenBlock = require('./secjs-token-block')
 const SECDatahandler = require('@sec-block/secjs-datahandler')
@@ -26,58 +27,16 @@ class SECTokenBlockChain {
    * generate genesis token block
    */
   _generateGenesisBlock () {
-    let extraData = 'Hello World'
-    if (process.env.secTest) {
-      extraData = 'Test Network Genesis Block'
-    }
-    if (this.chainName === 'SEC') {
-      return new SECTokenBlock({
-        Number: 0,
-        ReceiptRoot: SECUtils.KECCAK256_RLP.toString('hex'),
-        LogsBloom: SECUtils.zeros(256).toString('hex'),
-        MixHash: '',
-        StateRoot: SECUtils.KECCAK256_RLP.toString('hex'),
-        TimeStamp: 1537900000,
-        ParentHash: SECUtils.zeros(32).toString('hex'),
-        Beneficiary: '',
-        Difficulty: '',
-        ExtraData: 'SEC blockchain ' + extraData,
-        Nonce: '',
-        Transactions: [{
-          TxHash:
-          '4931dc83cb7eafb8367e5d933de24216a159337f54a2d7025d8e48c0ac73b75f',
-          TxReceiptStatus: 'success',
-          Version: '0.1',
-          TimeStamp: 1537900000,
-          TxFrom: '0000000000000000000000000000000000000000',
-          TxTo: '0000000000000000000000000000000000000001',
-          Value: '1500000000',
-          GasLimit: '0',
-          GasUsedByTxn: '0',
-          GasPrice: '0',
-          TxFee: '0',
-          Nonce: '0',
-          InputData: '1.5 billion initial tokens',
-          Signature: {}
-        }]
-      }).getBlock()
-    } else if (this.chainName === 'SEN') {
-      return new SECTokenBlock({
-        Number: 0,
-        ReceiptRoot: SECUtils.KECCAK256_RLP.toString('hex'),
-        LogsBloom: SECUtils.zeros(256).toString('hex'),
-        MixHash: SECUtils.zeros(32).toString('hex'),
-        StateRoot: SECUtils.KECCAK256_RLP.toString('hex'),
-        TimeStamp: 1537900000,
-        ParentHash: SECUtils.zeros(32).toString('hex'),
-        Beneficiary: SECUtils.zeros(20).toString('hex'),
-        Difficulty: '1',
-        ExtraData: 'SEN blockchain ' + extraData,
-        Nonce: SECUtils.zeros(8).toString('hex'),
-        Transactions: []
-      }).getBlock()
+    if (process.env.secTest && this.chainName === 'SEC') {
+      return new SECTokenBlock(geneData.secTestGeneBlock).getBlock()
+    } else if (process.env.secTest && this.chainName === 'SEN') {
+      return new SECTokenBlock(geneData.senTestGeneBlock).getBlock()
+    } else if (process.env.secTest === undefined && this.chainName === 'SEC') {
+      return new SECTokenBlock(geneData.secGeneBlock).getBlock()
+    } else if (process.env.secTest === undefined && this.chainName === 'SEN') {
+      return new SECTokenBlock(geneData.senGeneBlock).getBlock()
     } else {
-      throw new Error('Invalid chain name, should be either SEC or SEN')
+      throw new Error(`Invalid chain name: ${this.chainName}`)
     }
   }
 
