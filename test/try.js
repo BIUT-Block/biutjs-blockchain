a770144120a5216d6da885263c76ee51d291be73
Ming <ming1992liu@gmail.com>
2019-07-22 20:12:36 +0200

    Comment test

--- test/try.js
+++ test/try.js
@@ -5,39 +5,39 @@ const config = {
   // 'StateRoot': '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3'
 }
 const root = '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3'
-const accTree = new AccTreeDB(config)
-console.log(accTree.getRoot())
+// const accTree = new AccTreeDB(config)
+// console.log(accTree.getRoot())
 
-accTree.constructNewTree(root)
+// accTree.constructNewTree(root)
 
-accTree.putAccInfo('12345', ['1.1', 2], (err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('success')
-    console.log(accTree.getRoot())
-  }
-})
+// accTree.putAccInfo('12345', ['1.1', 2], (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('success')
+//     console.log(accTree.getRoot())
+//   }
+// })
 
-accTree.getAccInfo('12345', (err, data) => {
-  if (err) {
-    console.log('===============')
-    // console.log(err)
-    // accTree.constructNewTree(root)
-    accTree.getAccInfo('12345', (err, data) => {
-      if (err) {
-        console.log(accTree.getRoot())
-        console.log('2222222222')
-      } else {
-        console.log(accTree.getRoot())
-        console.log(data)
-      }
-    })
-  } else {
-    console.log('data')
-    console.log(data)
-  }
-})
+// accTree.getAccInfo('12345', (err, data) => {
+//   if (err) {
+//     console.log('===============')
+//     // console.log(err)
+//     // accTree.constructNewTree(root)
+//     accTree.getAccInfo('12345', (err, data) => {
+//       if (err) {
+//         console.log(accTree.getRoot())
+//         console.log('2222222222')
+//       } else {
+//         console.log(accTree.getRoot())
+//         console.log(data)
+//       }
+//     })
+//   } else {
+//     console.log('data')
+//     console.log(data)
+//   }
+// })
 
 accTree.checkRoot('2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3', (err, result) => {
   console.log(err)
