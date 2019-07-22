5ef3fbef379cdd726d88657e9b2b4bf585a50e72
Ming <ming1992liu@gmail.com>
2019-07-22 20:17:59 +0200

    Revert test

--- test/try.js
+++ test/try.js
@@ -1,45 +1,32 @@
-const AccTreeDB = require('../src/accTreeDB.js')
-
-const config = {
-  'DBPath': '../data/'
-  // 'StateRoot': '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3'
+const SECDataHandler = require('@sec-block/secjs-datahandler')
+const SECBlockchain = require('../src/index')
+const dbconfig = {
+  DBPath: process.cwd() + '/data',
+  ID: 'TestID'
 }
-const root = '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3'
-// const accTree = new AccTreeDB(config)
-// console.log(accTree.getRoot())
 
-// accTree.constructNewTree(root)
+const tokenDH = new SECDataHandler.TokenBlockChainDB(dbconfig)
+const tokenBlockChain = new SECBlockchain.SECTokenBlockChain(tokenDH)
 
-// accTree.putAccInfo('12345', ['1.1', 2], (err) => {
-//   if (err) {
-//     console.log(err)
-//   } else {
-//     console.log('success')
-//     console.log(accTree.getRoot())
-//   }
-// })
-
-// accTree.getAccInfo('12345', (err, data) => {
-//   if (err) {
-//     console.log('===============')
-//     // console.log(err)
-//     // accTree.constructNewTree(root)
-//     accTree.getAccInfo('12345', (err, data) => {
-//       if (err) {
-//         console.log(accTree.getRoot())
-//         console.log('2222222222')
-//       } else {
-//         console.log(accTree.getRoot())
-//         console.log(data)
-//       }
-//     })
-//   } else {
-//     console.log('data')
-//     console.log(data)
-//   }
-// })
-
-accTree.checkRoot('2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3', (err, result) => {
-  console.log(err)
-  console.log(result)
+tokenBlockChain.init((e) => {
+  if (e) {
+    console.log(e)
+  } else {
+    tokenBlockChain.accTree.getRoots((err, value) => {
+      if (err) console.log(err)
+      else {
+        console.log(value)
+      }
+    })
+  }
 })
+
+// const accConfig = {
+//   DBPath: '../data/test/tokenBlockChain'
+// }
+
+// let accTree = new SECDataHandler.AccTreeDB(accConfig)
+
+// accTree.getRoot((e, data) => {
+//   console.log(data)
+// })
